fafc67a776168dda64169af4891e0932293cfaab
terrotar <pedro_guedes93@hotmail.com>
2021-11-23 03:14:42 -0300

    route /write-message-on-image created; new_img saved in temporary folder and can be found with the response's name in route /get-image

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 
 from fastapi.responses import FileResponse, JSONResponse
 
+from PIL import BmpImagePlugin
+
 from .schemas import Message
 
-from . import crud
+from . import crud, settings
 
 
 # FastAPI instance
@@ -54,14 +56,17 @@ async def write_message(message: Message):
     file = crud.check_upload(message.filename)
     if(file):
 
-        # Get file and message's size
+        # Get file's bytes
         file_bytes = []
-
-        # Open image in bytes
         img = open(file, "rb")
         for i in list(bytes(img.read())):
-            i = format(i, '08b')
+            # i = format(i, '08b')
             file_bytes.append(i)
+
+        # Set a dot(.) in the final of message
+        set_dot = list(message.message)
+        if(set_dot[-1] != "."):
+            message.message = message.message + "."
 
         # Open message in bytes
         message_bytes = []
@@ -69,14 +74,62 @@ async def write_message(message: Message):
             i = format(i, '08b')
             message_bytes.append(i)
 
-        return {
-            "file_bytes_qtd": f"{len(file_bytes)}",
-            "file_bytes": f"{file_bytes}",
-            "First_least_significant_bit": f"{list(file_bytes[0])[-1]}",
-            "message_bytes_qtd": f"{len(message_bytes)}",
-            "message_bytes": f"{message_bytes}"}
+        # Split message in bits
+        message_bits = []
+        for byte in message_bytes:
+            byte = list(byte)
+            for i in byte:
+                message_bits.append(i)
 
-        # with open(file, 'rb') as buffer:
-        #    return {"binary_image": f"{buffer.read()}"}
+        # Get size of file_bytes equal size of message_bytes
+        file_message_bytes = file_bytes[:len(message_bytes)*8]
+        # Create the new binary with message in LSB
+        new_img = []
+        pos = 0
+        for img_byte in file_bytes:
+            if(img_byte in file_bytes[0:10]):
+                new_img.append(img_byte)
+            else:
+                if(pos < len(message_bits)):
+                    # Discard the LSB of first fyle byte
+                    img_byte = img_byte >> 1
+                    # Add message bit
+                    message_bit = int(message_bits[pos])
+                    img_byte = img_byte << 1 | message_bit
+                    new_img.append(img_byte)
+                    pos += 1
+                else:
+                    new_img.append(img_byte)
+                    pos += 1
+
+        # Set new_img to image and saves it in temporary folder
+        # with prefix = "new_"
+        input_image = BmpImagePlugin.BmpImageFile(file)
+        output_image = input_image.copy()
+        output_image.frombytes(bytes(new_img))
+        file = file.split(".")
+        file = file[0].split("/")
+        new_img_path = f"{settings.PATH}/new_{file[-1]}.bmp"
+        output_image.save(f"{new_img_path}")
+
+        new_img_bytes = []
+        for img_byte in new_img:
+            img_byte = format(img_byte, '08b')
+            new_img_bytes.append(img_byte)
+
+        return {"new_file": f"new_{file[-1]}"}
+        # return FileResponse(new_img_path)
+        """
+        return {
+            # "file_bytes_qtd": f"{len(file_bytes)}",
+            # "new_img_bytes": f"{new_img_bytes [0:8]}",
+            "new_img": f"{bytearray(new_img[1:-2])}",
+            # "file_LSB": f"{img_LSB}",
+            # "First_least_significant_bit": f"{list(file_bytes[0])[-1]}",
+            # "necessary_bytes_message": f"{len(message_bytes)*8}",
+            # "file_message_bytes": f"{file_message_bytes}",
+            # "message_bits": f"{message_bits}"}
+           # "message_bytes": f"{message_bytes}"}
+           """
     raise HTTPException(
         status_code=400, detail=f"File {message.filename} not found")
