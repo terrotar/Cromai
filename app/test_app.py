57b825f776184a69d8cb275e1950774d7a6d8c95
terrotar <pedro_guedes93@hotmail.com>
2021-11-24 04:51:23 -0300

    Finished tests in route /upload

--- app/test_app.py
+++ app/test_app.py
@@ -15,14 +15,25 @@ client = TestClient(app)
 
 
 # /upload
-def test_success_upload():
 
+
+# Success upload
+def test_success_upload():
     url = 'http://127.0.0.1:8000/upload'
     files = {'file': open('./imgs/dog_01.bmp', 'rb')}
     response = requests.post(url, files=files)
 
-    # assert response.status_code == 200
     assert response.status_code == 200
+
+
+# Extension not allowed
+def test_extension_not_allowed():
+    url = 'http://127.0.0.1:8000/upload'
+    files = {'file': open('./imgs/cat_not_bitmap.jpg', 'rb')}
+    response = requests.post(url, files=files)
+
+    assert response.status_code == 422
+
 
 # /get-image/{filename}
 
